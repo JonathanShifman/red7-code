2a67485e1ffd2e23bb663123f3dae3d75c9ad7eb
Jonathan <jonathan.shifman@gmail.com>
2019-06-03 21:31:02 +0300

    Displaying hand cards

--- client/src/app/components/app/app.component.ts
+++ client/src/app/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Card} from "../../classes/card";
 
 @Component({
@@ -6,6 +6,18 @@ import {Card} from "../../classes/card";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   card = new Card(1, 1);
+
+  handCards: Card[] = [];
+
+  ngOnInit() {
+    this.handCards.push(new Card(1, 1));
+    this.handCards.push(new Card(2, 2));
+    this.handCards.push(new Card(3, 3));
+    this.handCards.push(new Card(4, 4));
+    this.handCards.push(new Card(5, 5));
+    this.handCards.push(new Card(6, 6));
+    this.handCards.push(new Card(7, 7));
+  }
 }
