7aed6fe1562e387cf6f682b4c996c3b2d2bd5386
Jonathan <jonathan.shifman@gmail.com>
2019-06-03 22:28:15 +0300

    Designed registration component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './components/app/app.component';
 import { OpponentComponent } from './components/opponent/opponent.component';
 import { CardComponent } from './components/card/card.component';
 import { GameComponent } from './components/game/game.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { LobbyComponent } from './components/lobby/lobby.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OpponentComponent,
     CardComponent,
-    GameComponent
+    GameComponent,
+    RegistrationComponent,
+    LobbyComponent
   ],
   imports: [
     BrowserModule
