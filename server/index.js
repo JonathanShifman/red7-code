40a57cdfbb1f1ea5563c3d03b719ae3efc9ff7e8
Jonathan <jonathan.shifman@gmail.com>
2019-06-19 20:38:03 +0300

    Lobby entrance restrictions

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 const lobbyPlayers = [];
 const sockets = [];
+let diskData;
+fs.readFile('data.json', (err, data) => {
+    diskData = JSON.parse(data);
+});
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -24,8 +30,10 @@ io.on('connection', function(socket){
     });
 
     socket.on('auth',  storageData => {
-        console.log(storageData);
+        // console.log(storageData);
     });
+
+    socket.emit('lobby-players', lobbyPlayers);
 });
 
 const port = 5000;
@@ -33,9 +41,13 @@ http.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 function register(req, res) {
     const object = {
-        id: 1,
+        id: diskData['nextId'],
         name: req.body.name
     };
+
+    diskData['nextId'] = diskData['nextId'] + 1;
+    fs.writeFile('data.json', JSON.stringify(diskData), () => {});
+
     const responseObject = {
         name: req.body.name,
         token: jwt.sign(object, 'secretkey')
@@ -45,7 +57,21 @@ function register(req, res) {
 
 function enterGame(req, res) {
     const userInfo = jwt.decode(req.body.token);
-    lobbyPlayers.push(userInfo.name);
+    console.log('Attempting to add lobby player');
+
+    if (lobbyPlayers.length >= 4) {
+        console.log('Lobby is full');
+        res.status(200);
+        return;
+    }
+    for (let lobbyPlayer of lobbyPlayers) {
+        if (lobbyPlayer.id === userInfo.id) {
+            console.log('The player is already in the lobby');
+            res.status(200);
+            return;
+        }
+    }
+    lobbyPlayers.push(userInfo);
     for (const socket of sockets) {
         socket.emit('lobby-players', lobbyPlayers);
     }
